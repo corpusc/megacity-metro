8111b01aa7d378a1a0b38fc774494288f0a5093a
corpusc <corpus_callosum@hotmail.com>
2024-03-26 07:16:50 -0400

    TutorialScreen.cs   (not sure why it looks like everything got indented....same indention as original in VS2022)

--- Assets/Scripts/Gameplay/Client/UI/MonoBehaviours/Misc/TutorialScreen.cs
+++ Assets/Scripts/Gameplay/Client/UI/MonoBehaviours/Misc/TutorialScreen.cs
@@ -5,63 +5,63 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 using UnityEngine.UIElements;
 
-namespace Unity.MegacityMetro.UI
-{
-    /// <summary>
-    /// Tutorial Screen UI element
-    /// </summary>
-    [RequireComponent(typeof(UIDocument))]
-    public class TutorialScreen : MonoBehaviour
-    {
-        public static TutorialScreen Instance { get; private set; }
+namespace Unity.MegacityMetro.UI {
+	/// <summary>
+	/// Tutorial Screen UI element
+	/// </summary>
+	[RequireComponent(typeof(UIDocument))]
+	public class TutorialScreen : MonoBehaviour {
+		public static TutorialScreen Instance { get; private set; }
 
-        private VisualElement m_TutorialScreen;
-        private VisualElement m_SinglePlayerTutorial;
-        private VisualElement m_MultiplayerTutorial;
-        private VisualElement m_MobileTutorial;
-        private VisualElement m_MobileMultiplayerTutorial;
-        private VisualElement m_ShootButton;
-        private VisualElement m_LeaderboardInstructions;
-        private VisualElement m_JoystickHandler;
-        private VisualElement m_SpeedSlider;
-        private bool m_InTutorialScreen;
+		VisualElement m_TutorialScreen;
 
-        private void Awake()
-        {
-            if (Instance == null)
-            {
-                Instance = this;
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
-        }
+		/// <summary> [s]ingle [p]layer </summary>
+		VisualElement sp;
+		/// <summary> [m]ulti[p]layer </summary>
+		VisualElement mp;
+		/// <summary> [s]ingle [p]layer </summary>
+		VisualElement spMobile;
+		/// <summary> [m]ulti[p]layer </summary>
+		VisualElement mpMobile;
 
-        private void OnEnable()
-        {
-            var root = GetComponent<UIDocument>().rootVisualElement;
-            m_TutorialScreen = root.Q<VisualElement>("tutorial-screen");
-            m_SinglePlayerTutorial = root.Q<VisualElement>("tutorial-single-player");
-            m_MultiplayerTutorial = root.Q<VisualElement>("tutorial-multiplayer");
-            m_MobileTutorial = root.Q<VisualElement>("tutorial-mobile");
-            m_ShootButton = root.Q<VisualElement>("shoot-button");
-            m_LeaderboardInstructions = root.Q<VisualElement>("leaderboard-instructions");
-            m_JoystickHandler = root.Q<VisualElement>("handle");
-            m_SpeedSlider = root.Q<VisualElement>("speed-slider");
-        }
+		VisualElement m_ShootButton;
+		VisualElement m_LeaderboardInstructions;
+		VisualElement m_JoystickHandler;
+		VisualElement m_SpeedSlider;
+		bool active;
 
-        private void Start()
-        {
-            if (PlayerInfoController.Instance == null)
-                return;
 
-            ShowTutorial();
+
+		void Awake () {
+			if (Instance == null) {
+				Instance = this;
+			} else {
+				Destroy(gameObject);
+			}
+		}
+
+		void OnEnable () {
+			var root = GetComponent<UIDocument>().rootVisualElement;
+			m_TutorialScreen = root.Q<VisualElement>("tutorial-screen");
+			sp = root.Q<VisualElement>("tutorial-single-player");
+			mp = root.Q<VisualElement>("tutorial-multiplayer");
+			spMobile = root.Q<VisualElement>("tutorial-mobile");
+			m_ShootButton = root.Q<VisualElement>("shoot-button");
+			m_LeaderboardInstructions = root.Q<VisualElement>("leaderboard-instructions");
+			m_JoystickHandler = root.Q<VisualElement>("handle");
+			m_SpeedSlider = root.Q<VisualElement>("speed-slider");
+		}
+
+		void Start () {
+			if (PlayerInfoController.Instance == null)
+				return;
+
+			ShowTutorial();
 
 #if UNITY_ANDROID || UNITY_IPHONE
-            m_MobileTutorial.style.display = DisplayStyle.Flex;
-            m_SinglePlayerTutorial.style.display = DisplayStyle.None;
-            m_MultiplayerTutorial.style.display = DisplayStyle.None;
+            spMobile.style.display = DisplayStyle.Flex;
+            sp      .style.display = DisplayStyle.None;
+            mp      .style.display = DisplayStyle.None;
 
             if (PlayerInfoController.Instance.IsSinglePlayer)
             {
@@ -74,60 +74,51 @@ namespace Unity.MegacityMetro.UI
                 m_LeaderboardInstructions.style.display = DisplayStyle.Flex;
             }
 #else
-            m_MobileTutorial.style.display = DisplayStyle.None;
-            
-            if (PlayerInfoController.Instance.IsSinglePlayer)
-            {
-                m_SinglePlayerTutorial.style.display = DisplayStyle.Flex;
-                m_MultiplayerTutorial.style.display = DisplayStyle.None;
-            }
-            else
-            {
-                m_MultiplayerTutorial.style.display = DisplayStyle.Flex;
-                m_SinglePlayerTutorial.style.display = DisplayStyle.None;
-            }
+			spMobile.style.display = DisplayStyle.None;
+
+			if (PlayerInfoController.Instance.IsSinglePlayer) {
+				sp.style.display = DisplayStyle.Flex;
+				mp.style.display = DisplayStyle.None;
+			} else {
+				mp.style.display = DisplayStyle.Flex;
+				sp.style.display = DisplayStyle.None;
+			}
 #endif
-        }
+		}
 
-        public void ShowTutorial()
-        {
-            if (m_InTutorialScreen)
-                return;
+		public void ShowTutorial () {
+			if (active)
+				return;
 
-            m_TutorialScreen.style.display = DisplayStyle.Flex;
-            m_InTutorialScreen = true;
+			m_TutorialScreen.style.display = DisplayStyle.Flex;
+			active = true;
 
-            StartCoroutine(WaitForAnyInput());
+			StartCoroutine(WaitForAnyInput());
 #if UNITY_ANDROID || UNITY_IPHONE
             StartCoroutine(AnimateMobileTutorial());
 #endif
-        }
+		}
 
-        private void HideTutorial()
-        {
-            m_TutorialScreen.style.display = DisplayStyle.None;
-            m_InTutorialScreen = false;
-        }
+		void HideTutorial () {
+			m_TutorialScreen.style.display = DisplayStyle.None;
+			active = false;
+		}
 
-        private IEnumerator WaitForAnyInput()
-        {
-            while (m_InTutorialScreen)
-            {
-                InputSystem.onAnyButtonPress.CallOnce(_ => { HideTutorial(); });
-                yield return null;
-            }
-        }
-        
-        private IEnumerator AnimateMobileTutorial()
-        {
-            while (m_InTutorialScreen)
-            {
-                yield return new WaitForSeconds(1f);
-                m_JoystickHandler.transform.position = Random.insideUnitCircle * 50f;
-                m_SpeedSlider.transform.position = new Vector3(0, Random.Range(-100, 100), 0);
-                yield return new WaitForSeconds(1f);
-                m_JoystickHandler.transform.position = Vector3.zero;
-            }
-        }
-    }
+		IEnumerator WaitForAnyInput () {
+			while (active) {
+				InputSystem.onAnyButtonPress.CallOnce(_ => { HideTutorial(); });
+				yield return null;
+			}
+		}
+
+		IEnumerator AnimateMobileTutorial () {
+			while (active) {
+				yield return new WaitForSeconds(1f);
+				m_JoystickHandler.transform.position = Random.insideUnitCircle * 50f;
+				m_SpeedSlider.transform.position = new Vector3(0, Random.Range(-100, 100), 0);
+				yield return new WaitForSeconds(1f);
+				m_JoystickHandler.transform.position = Vector3.zero;
+			}
+		}
+	}
 }
